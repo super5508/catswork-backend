0601fe524b5b610f008eac9aff05557cffcca7ad
super5508 <srdev5008@gmail.com>
2019-08-02 23:58:01 +0530

    changed user authentication

--- src/schema/catworksAuthentication.js
+++ src/schema/catworksAuthentication.js
@@ -1,7 +1,6 @@
-// Important Note: for security purpose, wont be adding any relation with other Schema with authentication 
-// If wanted can move to simple api route as well 
-const graphql = require("graphql")
 
+const graphql = require("graphql")
+const { getSelectedThingFromTable } = require('../helpers/sql')
 const { 
   GraphQLEnumType,
   GraphQLObjectType,
@@ -27,6 +26,22 @@ const userAuthentication = new GraphQLObjectType({
     },
     isVerified: {
       type: GraphQLInt
+    },
+    userInformation: {
+      type: require('./catworksPersonal').userPersonalType, 
+      resolve: async (parent, args, request) => {
+        //TODO: Error Handling
+        console.log(`Parent User Id:`, parent[0].userId) //Since ID is going to be same everywhere
+        const userData = await getSelectedThingFromTable('CatsWork_personal', `userId`, `${parent[0].userId}`)
+        return userData[0]
+      }
+    },
+    userDashboard: { // Since user can have multiple list of enteries, using list
+      type: new GraphQLList(require('./catworksDashboard').userDashboardType), 
+      resolve: async (parent, args, request) => {
+        const userData = await getSelectedThingFromTable('CatsWork_dashboard', `userId`,  `${parent.userId}`)
+        return userData
+      }
     }
   })
 })
