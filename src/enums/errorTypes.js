4b5a47a52196f3b19f0ab2b0728ee54d9d9b470b
super5508 <srdev5008@gmail.com>
2019-08-06 17:20:16 +0530

    comma error fixed

--- src/enums/errorTypes.js
+++ src/enums/errorTypes.js
@@ -7,7 +7,7 @@ const ErrorTypes = {
   INVALID_LOGIN: "401/invalid-login",
   INCORRECT_PASSWORD: "401/incorrect-password",
   UNVERIFIED_EMAIL: "401/unverified-email",
-  AUTHENTICATION_OTP_MESSSGE_PROBLEM: '500/unable-to-send-authentication-message'
+  AUTHENTICATION_OTP_MESSSGE_PROBLEM: '500/unable-to-send-authentication-message',
   SQL_ERROR: `500/problem-crating-new-user`, 
   INCORRECT_OTP: '400/Incorrect-otp-entered'
 }
