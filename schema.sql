3d546b05eab7a2ae6919c7038dc2bb48c539a6b2
super5508 <srdev5008@gmail.com>
2019-08-22 15:11:45 +0530

    latest SQL dumb

--- schema.sql
+++ schema.sql
@@ -10,7 +10,7 @@
  Target Server Version : 80016
  File Encoding         : 65001
 
- Date: 13/08/2019 04:08:29
+ Date: 22/08/2019 15:09:33
 */
 
 SET NAMES utf8mb4;
@@ -25,13 +25,13 @@ CREATE TABLE `CatsWork_authentication` (
   `userId` int(11) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) DEFAULT NULL,
-  `generated_otp` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `updatedAt` datetime(6) NOT NULL ON UPDATE CURRENT_TIMESTAMP(6),
-  `createdAt` datetime(6) NOT NULL,
+  `generated_otp` varchar(255) DEFAULT NULL,
+  `updatedAt` datetime NOT NULL,
+  `createdAt` datetime NOT NULL,
   `linkedinRefreshToken` varchar(1000) DEFAULT NULL,
   `activeStep` tinyint(1) DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for CatsWork_dashboard
@@ -40,41 +40,42 @@ DROP TABLE IF EXISTS `CatsWork_dashboard`;
 CREATE TABLE `CatsWork_dashboard` (
   `personId` int(11) NOT NULL AUTO_INCREMENT,
   `userId` int(11) NOT NULL,
-  `first` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `last` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `first` varchar(255) DEFAULT NULL,
+  `last` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `company` varchar(255) DEFAULT NULL,
-  `position` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `position` varchar(255) DEFAULT NULL,
   `industry` varchar(255) DEFAULT NULL,
-  `phone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
   `location` varchar(255) DEFAULT NULL,
   `education` varchar(255) DEFAULT NULL,
   `hometown` varchar(255) DEFAULT NULL,
-  `extracurriculars` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `extracurriculars` varchar(255) DEFAULT NULL,
   `website` varchar(255) DEFAULT NULL,
   `notes` varchar(255) DEFAULT NULL,
   `source` varchar(255) DEFAULT NULL,
   `sourceCustom` varchar(300) DEFAULT NULL,
-  `createdAt` timestamp(6) NOT NULL,
-  `updatedAt` timestamp(6) NOT NULL ON UPDATE CURRENT_TIMESTAMP(6),
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
   PRIMARY KEY (`personId`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for CatsWork_notification
 -- ----------------------------
 DROP TABLE IF EXISTS `CatsWork_notification`;
 CREATE TABLE `CatsWork_notification` (
-  `notificationId` tinyint(11) NOT NULL,
-  `userId` tinyint(11) NOT NULL,
+  `notificationId` tinyint(11) NOT NULL AUTO_INCREMENT,
+  `userId` int(11) NOT NULL,
   `message` varchar(255) NOT NULL,
-  `type` tinyint(11) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
   `personId` tinyint(11) DEFAULT NULL,
   `activity` tinyint(11) DEFAULT NULL,
-  `createdAt` timestamp NOT NULL,
-  `updatedAt` timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `date` timestamp NULL DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
   PRIMARY KEY (`notificationId`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for CatsWork_personal
@@ -83,18 +84,18 @@ DROP TABLE IF EXISTS `CatsWork_personal`;
 CREATE TABLE `CatsWork_personal` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `userId` int(11) NOT NULL,
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `name` varchar(255) NOT NULL,
   `gradYear` int(4) DEFAULT NULL,
-  `gradMonth` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `gradMonth` varchar(255) DEFAULT NULL,
   `school` varchar(255) DEFAULT NULL,
   `degree` varchar(255) DEFAULT NULL,
   `major` varchar(255) DEFAULT NULL,
   `gender` tinyint(1) DEFAULT NULL,
-  `desiredIndustry` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `createdAt` timestamp NOT NULL,
-  `updatedAt` timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `desiredIndustry` varchar(255) DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for catworks_activity
@@ -108,9 +109,9 @@ CREATE TABLE `catworks_activity` (
   `activity` varchar(255) DEFAULT NULL,
   `status` tinyint(11) DEFAULT NULL,
   `date` varchar(255) DEFAULT NULL,
-  `createdAt` timestamp NOT NULL,
-  `updatedAt` timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
   PRIMARY KEY (`activityId`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
 
 SET FOREIGN_KEY_CHECKS = 1;
